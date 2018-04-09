884867df8ee223ba67753f8c688cba8f54a8d096
Jean Carlo <jeancarlo.flores@vasslatam.com>
2018-04-08 22:32:01 -0500

    Componentes reutilizables

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
+import ArtistBox from './artist/artistbox'
 
 export default class App extends React.Component {
     render() {
+        const name = "Jean Carlo"
+        const image = 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png'
+        const likes = 200
+        const comments = 140
         return (
             <View style={styles.container}>
-               <View style={[styles.box, styles.red]}/>
+               <ArtistBox/>
+                <View>
+                </View>
+                {/*            
+            <View style={[styles.box, styles.red]}/>
                 <View style={styles.yellow}/>
                 <View style={styles.green}/>
-               
-
-
-
-
-
-
                 <Button
                     onPress={onPressLearnMore}
                     title="Learn More"
@@ -25,11 +28,12 @@ export default class App extends React.Component {
                 <Text>Open up App.js to start working on your app!</Text>
                 <Text>Changes you make will automatically reload.</Text>
                 <Text>Shake your phone to open the developer menu.</Text>
+                */}
             </View>
         );
     }
 }
-showAlert = () => {
+/*showAlert = () => {
     // Works on both iOS and Android
     Alert.alert(
         'Alert Title',
@@ -53,41 +57,78 @@ onPressLearnMore = event => {
             { text: 'OK', onPress: () => console.log('OK Pressed') },
         ],
         { cancelable: false }
-    )*/
+    )
     showAlert()
-}
+}*/
+
 const styles = StyleSheet.create({
     container: {
-        //1 : para que ocupe toda la pantalla, 0 se ajusta al tamaño de los hijos
-        flex: 0,
-        //se agrupan en una fila
+        flex: 1,
+        backgroundColor: 'lightgray',
+        paddingTop: 50,
+    },
+    artistBox: {
+        backgroundColor: 'white',
+        flexDirection: 'row'
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    info: {
+        flex: 1,
         flexDirection: 'column',
-        //como se alinean los elementos en el eje que yo indiqué
-        justifyContent: 'center',
-        //alinear los elementos
         alignItems: 'center',
-        backgroundColor: '#fff',
-        flexWrap: 'wrap'
+        justifyContent: 'center'
     },
-    box: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'black'
+    name: {
+        fontSize: 20,
+        marginTop: 10,
+        color: '#333'
     },
-    red: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'red'
-   },
-    green: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'green'
+    row: {
+        flexDirection: 'row',
+        marginHorizontal: 30,
+        marginTop: 15
     },
-    yellow: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'yellow'
+    iconContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    count: {
+        color: 'gray'
     }
+    /* container: {
+         //1 : para que ocupe toda la pantalla, 0 se ajusta al tamaño de los hijos
+         flex: 0,
+         //se agrupan en una fila
+         flexDirection: 'column',
+         //como se alinean los elementos en el eje que yo indiqué
+         justifyContent: 'center',
+         //alinear los elementos
+         alignItems: 'center',
+         backgroundColor: '#fff',
+         flexWrap: 'wrap'
+     },
+     box: {
+         width: 100,
+         height: 100,
+         backgroundColor: 'black'
+     },
+     red: {
+         width: 100,
+         height: 100,
+         backgroundColor: 'red'
+     },
+     green: {
+         width: 100,
+         height: 100,
+         backgroundColor: 'green'
+     },
+     yellow: {
+         width: 100,
+         height: 100,
+         backgroundColor: 'yellow'
+     }*/
 
 });
